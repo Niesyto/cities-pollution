862ca04dcd5b0b1760cb75a3271deab6f2169296
niesyto <jarekniesyto@gmail.com>
2019-08-09 21:15:33 +0200

    Fixed CitiesList loading

--- react-test-create-react-app/src/CitiesList.js
+++ react-test-create-react-app/src/CitiesList.js
@@ -1,69 +1,32 @@
-import React from 'react';
-import CitiesListElement from './CitiesListElement';
-import { Accordion } from 'react-light-accordion';
+import React, { useState, useEffect, useCallback } from "react";
+import CitiesListElement from "./CitiesListElement";
+import { Accordion } from "react-light-accordion";
 
-class CitiesList extends React.Component{
+function CitiesList({ country, parameter }) {
+  const [cities, setCities] = useState(undefined);
 
-    constructor(props){
-        super(props);
-    this.state = {
-      parameter: "",
-      country: "",
-      cities: [],
-      isLoaded:false
-      };
-    }
+  const getCities = useCallback(() => {
+    const url = `https://api.openaq.org/v1/latest?order_by=value&sort=desc&limit=10&parameter=${parameter}&country=${country}`;
 
-  componentDidUpdate(){
-   
-    fetch(`https://api.openaq.org/v1/latest?order_by=value&sort=desc&limit=10&parameter=${this.props.parameter}&country=${this.props.country}`, {mode: 'cors'})
-      .then(res => res.json())
-      .then(
-        (result) => {
-            this.setState({
-                country : this.props.country,
-                parameter : this.props.parameter,
-                cities : result.results,
-                isLoaded : true
-            });
-        });
-  }
+    fetch(url, { mode: "cors" })
+      .then(_ => _.json())
+      .then(({ results }) => setCities(results));
+  }, [country, parameter]);
 
+  useEffect(() => {
+    getCities();
+  }, [getCities]);
 
-  static getDerivedStateFromProps(props, state) {
-    if ((props.country !== state.country) || (props.parameter !== state.parameter)) {
-      return {
-        isLoaded:false
-      };
-    }
+  if (cities === undefined) return <div>Loading data...</div>;
+  if (cities === null || (cities && cities.length === 0))
+    return <div>There is no data...</div>;
 
-    // Return null if the state hasn't changed
-    return null;
-  }
-
-
-    render(){  
-        const {cities,isLoaded} = this.state;
-        if(!isLoaded){
-            return(
-            <div> Loading... </div>
-            )
-        } else {
-            return(
-                <div className="w-10 p-3">
-                {cities.map(city => (
-                    <Accordion atomic={true} key={city.location}>
-                        <CitiesListElement
-                        cityName={city.city}
-                        />
-                    </Accordion>
-                ))}
-                </div>
-            );
-        }
-    }
+  const citiesElements = cities.map(city => (
+    <Accordion atomic={true} key={city.location}>
+      <CitiesListElement cityName={city.city} />
+    </Accordion>
+  ));
+  return <div className="w-10 p-3">{citiesElements}</div>;
 }
 
-
-  export default CitiesList;
-
+export default CitiesList;
